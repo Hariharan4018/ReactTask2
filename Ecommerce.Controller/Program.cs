d7f336322d18a006392508c1f0eea1f4b3ef9767
Hariharan4018 <140181589+Hariharan4018@users.noreply.github.com>
2025-09-15 08:03:49 +0530

    Create Program.cs

---
+++ Ecommerce.Controller/Program.cs
@@ -0,0 +1,79 @@
+
+using AutoMapper;
+using Ecommerce.DAL;
+using Ecommerce.DAL.ECommerceContext;
+using Ecommerce.DAL.Interfaces;
+using Ecommerce.Service.Interfaces;
+using Ecommerce.Service.Mapper;
+using Ecommerce.Service.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Ecommerce.Controller
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+           
+            builder.Services.AddControllers();
+            builder.Services.AddAutoMapper(typeof(MapperProfile).Assembly);
+            builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+            builder.Services.AddScoped<IAuthService, AuthServices>();
+            builder.Services.AddScoped<IAuthDAL, AuthDAL>();
+            builder.Services.AddDbContext<EcommerceDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetSection("ConnectionStrings")["SqlServer"]);
+            });
+
+            var jwtSettings = builder.Configuration.GetSection("Jwt");
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; 
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(option =>
+            {
+                option.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = jwtSettings["Issuer"],
+                    ValidAudience = jwtSettings["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+            builder.Services.AddAuthorization();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
