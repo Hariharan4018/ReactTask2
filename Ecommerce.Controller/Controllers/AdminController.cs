5dc78fd53f8cdc2772d93d91171c9423b4ed1257
Hariharan4018 <140181589+Hariharan4018@users.noreply.github.com>
2025-09-15 08:05:14 +0530

    Add files via upload

---
+++ Ecommerce.Controller/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using Ecommerce.Service.DTOs.ResponceDTOs;
+using Ecommerce.Service.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controller.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private IAdminServices _adminServices;
+        public AdminController(IAdminServices adminServices) { 
+        _adminServices = adminServices;
+        }
+        [HttpGet("GetAllCustomers")]
+        [Authorize(Roles = "Admin,SuperAdmin")]
+
+        public async Task<ActionResult<GenericApiResponceDTO<List<CustomerListDTO?>>>> GetAllCustomers()
+        {
+            var data = await _adminServices.GetAllCustomers();
+            if (!data.Success)
+            {
+                return NoContent();
+            }
+            if (data.Success)
+            {
+                return Ok(data);
+            }
+            return BadRequest(data);
+        }
+
+    }
+}
---
+++ Ecommerce.Controller/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using Ecommerce.Service.DTOs.RequestDTOs;
+using Ecommerce.Service.DTOs.ResponceDTOs;
+using Ecommerce.Service.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controller.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<GenericApiResponceDTO<ResponseDTO>>> UserRegister([FromBody] CustomerRegisterDTO customerData)
+        {
+            var data = await _authService.UserRegister(customerData);
+            return Ok(data);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<GenericApiResponceDTO<ResponseDTO?>>> Login([FromBody] LoginCredentialsDTO loginCredentialsDTO)
+        {
+            var data = await _authService.Login(loginCredentialsDTO);
+
+            if (data.Success)
+            {
+                return Ok(data);
+            }
+            if (data.Data == null)
+            {
+                return NotFound(data);
+            }
+            return BadRequest(data);
+
+        }
+
+    }
+}
